a0c1f87a388381a3609dde65d93e92513095d4c0
MarvinHongX <marvinhongx@gmail.com>
2024-11-12 13:14:34 +0900

    Feature Enhancement

--- pages/Web3.vue
+++ pages/Web3.vue
@@ -7,9 +7,6 @@ definePageMeta({
 <template>
     <div class="flex flex-column" style="width:100%">
         <SectionCenterA />
-
-        <SectionFilecoinNode />
-
         <SectionCenterADetail />
         <SectionMainComponentsTop />
         <SectionIpfs />
